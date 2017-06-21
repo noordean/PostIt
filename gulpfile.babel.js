54b03e6767d2972f08b347d685abf8193a7fa598
Nurudeen Ibrahim <ebroyeem90@gmail.com>
2017-06-21 16:06:21 +0100

    Update travis.yml with coveralls

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import nodemon from 'gulp-nodemon';
 import jasmine from 'gulp-jasmine';
 import exit from 'gulp-exit';
 import path from 'path';
+import istanbul from 'gulp-istanbul';
+import coveralls from 'gulp-coveralls';
 
 gulp.task('transpile', () =>
   gulp.src(['server/**/*.js', '!server/dist/**'])
@@ -24,6 +26,23 @@ gulp.task('run-tests', ['transpile'], () => {
   .pipe(exit());
 });
 
+// the coverage task depends on 'pre-test' and 'test' to give coverage report
+gulp.task('pre-test', () =>
+  gulp.src(['server/dist/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+);
 
+gulp.task('test', ['pre-test'], () =>
+  gulp.src([path.join('server', 'dist', 'tests', 'route-test.js')])
+    .pipe(jasmine())
+    .pipe(istanbul.writeReports())
+);
+
+gulp.task('coverage', ['test'], () => {
+  gulp.src('coverage/**/lcov.info')
+    .pipe(coveralls())
+    .pipe(exit());
+});
 
 gulp.task('default', ['run-tests']);

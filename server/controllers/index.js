f79a5e3454e49d5b9fd55d3cdfe11ce7718bee47
noordean <ebroyeem90@gmail.com>
2017-07-23 18:21:20 +0100

    fix API error of returning 404

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -26,20 +26,20 @@ export default class Controller {
     const password = req.body.password;
     const email = req.body.email;
     if (password === undefined) {
-      res.status(401).json({ message: 'Password must be supplied' });
+      res.json({ message: 'Password must be supplied' });
     } else if (/^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z\d]{5,12}$/.test(password) === false) {
-      res.status(401).json({ message: 'Password must be alphanumeric and should contain 5-12 characters' });
+      res.json({ message: 'Password must be alphanumeric and should contain 5-12 characters' });
     } else {
       const hashedPassword = bcrypt.hashSync(password, salt);
       userDbInstance.saveUser(username, hashedPassword, email, (user) => {
         if (Array.isArray(user)) {
           if (user[1] === false) {
-            res.status(401).json({ message: 'You already have an existing account. Kindly go and login' });
+            res.json({ message: 'You already have an existing account. Kindly go and login' });
           } else {
-            res.status(200).json({ message: 'Registration successful', id: user[0].id, username: user[0].username, email: user[0].email });
+            res.json({ message: 'Registration successful', id: user[0].id, username: user[0].username, email: user[0].email });
           }
         } else {
-          res.status(401).json({ message: user });
+          res.json({ message: user });
         }
       });
     }
@@ -55,11 +55,11 @@ export default class Controller {
     const username = req.body.username;
     const password = req.body.password;
     if (username === undefined || password === undefined) {
-      res.status(401).json({ message: 'You need to provide username and password' });
+      res.json({ message: 'You need to provide username and password' });
     } else {
       userDbInstance.getUser(username, (user) => {
         if (user.length === 0) {
-          res.status(404).json({ message: 'Invalid user!' });
+          res.json({ message: 'Invalid user!' });
         } else {
           let token = '';
           if (bcrypt.compareSync(password, user[0].password)) {
@@ -67,9 +67,9 @@ export default class Controller {
             token = jwt.sign(payload, 'nuruuuuuuu', {
               expiresIn: '1h'
             });
-            res.status(200).json({ message: 'You are now logged in', id: user[0].id, user: username, email: user[0].email, token });
+            res.json({ message: 'You are now logged in', id: user[0].id, user: username, email: user[0].email, token });
           } else {
-            res.status(404).json({ message: 'Incorrect password' });
+            res.json({ message: 'Incorrect password' });
           }
         }
       });
@@ -86,21 +86,21 @@ export default class Controller {
     const groupName = req.body.groupName;
     const token = req.body.token;
     if (groupName === undefined || token === undefined) {
-      res.status(401).json({ message: 'The group-name and your logged-in token must be specified' });
+      res.json({ message: 'The group-name and your logged-in token must be specified' });
     } else if (groupName.trim().length === 0) {
-      res.status(401).json({ message: 'The group-name cannot be empty' });
+      res.json({ message: 'The group-name cannot be empty' });
     } else {
       jwt.verify(token, 'nuruuuuuuu', (err, decode) => {
         if (decode !== undefined) {
           groupDbInstance.createGroup(groupName, decode.username, (group) => {
             if (group[1] === false) {
-              res.status(401).json({ message: 'There is already an existing group with this name' });
+              res.json({ message: 'There is already an existing group with this name' });
             } else {
-              res.status(200).json({ message: 'Group successfully created', id: group[0].id, name: group[0].groupname, members: group[0].groupmembers, createdby: group[0].createdby });
+              res.json({ message: 'Group successfully created', id: group[0].id, name: group[0].groupname, members: group[0].groupmembers, createdby: group[0].createdby });
             }
           });
         } else {
-          res.status(401).json({ message: 'Access denied!. Kindly login before creating group' });
+          res.json({ message: 'Access denied!. Kindly login before creating group' });
         }
       });
     }
@@ -117,26 +117,26 @@ export default class Controller {
     const username = req.body.username;
     const token = req.body.token;
     if (req.params.groupID === undefined || req.body.username === undefined || token === undefined) {
-      res.status(401).json({ message: 'You need to provide the group-id, your logged-in token and the username to add' });
+      res.json({ message: 'You need to provide the group-id, your logged-in token and the username to add' });
     } else if (id.trim().length === 0 || username.trim().length === 0) {
-      res.status(401).json({ message: 'Group-id and username cannot be empty' });
+      res.json({ message: 'Group-id and username cannot be empty' });
     } else if (isNaN(id)) {
-      res.status(401).json({ message: 'The supplied id must be an integer' });
+      res.json({ message: 'The supplied id must be an integer' });
     } else {
       groupDbInstance.getGroupById(id, (group) => {
         if (group.length === 0) {
-          res.status(404).json({ message: 'Invalid group id' });
+          res.json({ message: 'Invalid group id' });
         } else {
           userDbInstance.getUser(username, (user) => {
             if (user.length === 0) {
-              res.status(404).json({ message: 'Invalid user detected' });
+              res.json({ message: 'Invalid user detected' });
             } else {
               jwt.verify(token, 'nuruuuuuuu', (err, decode) => {
                 if (decode !== undefined) {
                   groupDbInstance.addUserToGroup(id, username);
-                  res.status(200).json({ message: 'User successfully added' });
+                  res.json({ message: 'User successfully added' });
                 } else {
-                  res.status(401).json({ message: 'Access denied!. Kindly login before adding user' });
+                  res.json({ message: 'Access denied!. Kindly login before adding user' });
                 }
               });
             }
@@ -157,23 +157,23 @@ export default class Controller {
     const message = req.body.message;
     const token = req.body.token;
     if (groupID === undefined || message === undefined || token === undefined) {
-      res.status(401).json({ message: 'You need to provide the group-id, your logged-in token and the message' });
+      res.json({ message: 'You need to provide the group-id, your logged-in token and the message' });
     } else if (groupID.trim().length === 0 || message.trim().length === 0) {
-      res.status(401).json({ message: 'group-id or message cannot be empty' });
+      res.json({ message: 'group-id or message cannot be empty' });
     } else if (isNaN(groupID)) {
-      res.status(401).json({ message: 'The supplied id must be an integer' });
+      res.json({ message: 'The supplied id must be an integer' });
     } else {
       groupDbInstance.getGroupById(groupID, (group) => {
         if (group.length === 0) {
-          res.status(404).json({ message: 'Invalid group id' });
+          res.json({ message: 'Invalid group id' });
         } else {
           jwt.verify(token, 'nuruuuuuuu', (err, decode) => {
             if (decode !== undefined) {
               messageDbInstance.postMessage(groupID, decode.username, message, (msg) => {
-                res.status(200).json({ message: 'Message posted successfully', groupID: msg.groupid, postedby: msg.postedby, content: msg.message });
+                res.json({ message: 'Message posted successfully', groupID: msg.groupid, postedby: msg.postedby, content: msg.message });
               });
             } else {
-              res.status(401).json({ message: 'Access denied!. Kindly login before posting message' });
+              res.json({ message: 'Access denied!. Kindly login before posting message' });
             }
           });
         }
@@ -191,21 +191,21 @@ export default class Controller {
     const groupID = req.params.groupID;
     const token = req.headers.token;
     if (groupID === undefined || groupID.trim().length === 0 || token === undefined) {
-      res.status(401).json({ message: 'group-id and your logged-in token must be provided' });
+      res.json({ message: 'group-id and your logged-in token must be provided' });
     } else if (isNaN(groupID)) {
-      res.status(401).json({ message: 'The supplied id must be an integer' });
+      res.json({ message: 'The supplied id must be an integer' });
     } else {
       groupDbInstance.getGroupById(groupID, (group) => {
         if (group.length === 0) {
-          res.status(404).json({ message: 'Invalid group id' });
+          res.json({ message: 'Invalid group id' });
         } else {
           jwt.verify(token, 'nuruuuuuuu', (err, decode) => {
             if (decode !== undefined) {
               messageDbInstance.getMessages(groupID, (messages) => {
-                res.status(200).json(messages);
+                res.json(messages);
               });
             } else {
-              res.status(401).json({ message: 'Access denied!. Kindly login before viewing messages' });
+              res.json({ message: 'Access denied!. Kindly login before viewing messages' });
             }
           });
         }

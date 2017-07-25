f8dfb339505fe564c6c576ed24214f66aff747a6
noordean <ebroyeem90@gmail.com>
2017-07-25 20:25:52 +0100

    setup a modal for creating group

--- client/components/header.js
+++ client/components/header.js
@@ -8,16 +8,42 @@ export default class Header extends Component{
     localStorage.removeItem('user');
     browserHistory.push('/');
   }
+  getAuto() {
+    alert(this.refs.autoInput.innerHTML);
+  }
   render(){
     const userHeader = (
-      <div>
-        <div id="modal1" className="modal">
-          <div className="modal-content">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
+          <div>
+            <div id="modal1" className="modal">
+            <div className="modal-content">
+              <form className="group-form">
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input id="password" type="text"/>
+                    <label htmlFor="password">Group Name</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <textarea id="textarea1" className="materialize-textarea white"></textarea>
+                    <label htmlFor="password">Description</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                  <div id="chip" className="chips chips-autocomplete"></div>
+                  <div id="getChips" ref="autoInput"></div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <a href="#" className="btn waves-effect waves-light col s12 red darken-4" onClick={this.getAuto.bind(this)}>Create</a>
+                </div>
+              </div>
+            </form>
           </div>
           <div className="modal-footer">
-            <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Create</a>
+            <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Cancel</a>
           </div>
         </div>
         <ul id="dropdown1" className="dropdown-content">
@@ -28,7 +54,7 @@ export default class Header extends Component{
         <div className="nav-wrapper red darken-4">
           <Link id="navLogo" to={localStorage.user ? "/dashboard" : "/"} className="brand-logo left">PostIt</Link>
           <ul className="right">
-            <li><a class="waves-effect waves-light btn modal-trigger" href="#modal1">Create Group</a></li>
+            <li><a className="waves-effect waves-light btn modal-trigger red darken-4" href="#modal1">Create Group</a></li>
             <li><a className="dropdown-button" href="" data-activates="dropdown1">{localStorage.user ? JSON.parse(localStorage.user).user : ''}<i className="material-icons right">arrow_drop_down</i></a></li>
           </ul>
         </div>
--- client/src/public/js/style.js
+++ client/src/public/js/style.js
@@ -1,4 +1,24 @@
 $(document).ready(() => {
   $('.slider').slider({full_width: true});
   $('.modal').modal();
+ $('.chips-autocomplete').material_chip({
+    placeholder: 'Enter a username',
+    secondaryPlaceholder: 'Add members',
+    autocompleteOptions: {
+      data: {
+        'Steven': null,
+        'Busayo': null,
+        'Google': null,
+        'Dunni': null,
+        'Lanre': null,
+        'Sholape': null
+      },
+      limit: Infinity,
+      minLength: 1,
+    }
+  });
+
+$('.chips').on('chip.add', (e, chip) => {
+   $('#getChips').append(chip.tag + ',');
+ });
 });

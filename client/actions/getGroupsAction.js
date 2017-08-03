bde726dbb0c6d61e24c2c6ec1f8723f3122954a8
noordean <ebroyeem90@gmail.com>
2017-08-03 19:03:03 +0100

    add limit to groupAction's endpoint

--- client/actions/getGroupsAction.js
+++ client/actions/getGroupsAction.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const getGroups = (offset) => {
+export const getGroups = (offset, limit) => {
   return (dispatch) => {
     dispatch({ type: 'GET_GROUPS_BEGINS' });
-    axios.get('https://postit-api.herokuapp.com/api/groups/' + JSON.parse(localStorage.user).user + '/' + offset)
+    axios.get('https://postit-api.herokuapp.com/api/groups/' + JSON.parse(localStorage.user).user + '/' + offset + '/' + limit)
       .then((response) => {
         dispatch({ type: 'GOT_GROUPS', payload: response.data });
       })
